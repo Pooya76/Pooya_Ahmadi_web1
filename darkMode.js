ef339a15130c2578cc9aaea7aba2eb978fd4ff69
unknown <pooya.ahmadi1376@gmail.com>
2021-11-17 15:09:32 +0330

    Assignment 6 requirements

---
+++ darkMode.js
@@ -0,0 +1,9 @@
+
+function changeMode(e){
+	var isChecked = document.getElementById("toggle").checked;
+	if(isChecked == true){
+		document.getElementById('indexCSS').href='darkMode.css';
+	}else{
+		document.getElementById('indexCSS').href='basic.css';
+	}
+}
